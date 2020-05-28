67bcecd6e14271781d8ccfb7e6d36e6df07f9bda
Oleg Borodin <onborodin@gmail.com>
2020-05-28 20:29:15 +0200

    add: set/get token to/from cookie; autodetect alg type from token header

--- server/jwt/jwt.go
+++ server/jwt/jwt.go
@@ -8,9 +8,11 @@ import (
     "time"
     "net/http"
     "strings"
-    "bytes"
+    //"bytes"
     "errors"
     "fmt"
+    "encoding/json"
+    "encoding/base64"
 
     "github.com/gin-gonic/gin"
     "github.com/lestrrat-go/jwx/jwa"
@@ -63,6 +65,8 @@ func (this *Controller) Login(context *gin.Context) {
         return
     }
 
+    context.SetCookie("token", string(signature), int(time.Duration(time.Hour).Seconds()), "/", "unix7.org", false, true)
+
     result := loginResult{
         Token: string(signature),
     }
@@ -76,32 +80,84 @@ func New(config *config.Config) *Controller {
 }
 
 func JwtAuthMiddleware(context *gin.Context) {
+
+    var signature string
+    var err error
+
     authHeader := context.Request.Header.Get("Authorization")
-    signature, err := parseAuthHeader(authHeader)
-    if err != nil {
-        response := controller.Response{
-            Error: true,
-            Message: fmt.Sprintf("parse auth header error: %s", err),
-            Result: "",
+
+
+    if strings.Contains(authHeader, "Bearer") {
+        signature, err = parseAuthHeader(authHeader)
+        if err != nil {
+            controller.AbortContext(context, http.StatusUnauthorized,
+                            errors.New(fmt.Sprintf("authorization error: %s", err)))
+            return
         }
-        context.JSON(http.StatusUnauthorized, response)
-        context.Abort()
-        return
+    } else {
+        signature, err = context.Cookie("token")
+        if err != nil {
+            controller.AbortContext(context, http.StatusUnauthorized,
+                            errors.New(fmt.Sprintf("authorization error: %s", err)))
+            return
+        }
     }
 
-    _, err = jwt.Parse(bytes.NewReader([]byte(signature)), jwt.WithVerify(jwa.HS256, []byte("secretword")))
+    alg, _ := getJwsAlg(signature)
+    _, err = jwt.Parse(strings.NewReader(signature), jwt.WithVerify(alg, []byte("secretword")))
+
+    /* there you will be write right control */
 
     if err != nil {
-        response := controller.Response{
-            Error: true,
-            Message: fmt.Sprintf("parse auth header error: %s", err),
-            Result: "",
-        }
-        context.JSON(http.StatusUnauthorized, response)
-        context.Abort()
+        controller.AbortContext(context, http.StatusUnauthorized,
+                            errors.New(fmt.Sprintf("authorization error: %s", err)))
         return
     }
     context.Next()
+}
+
+
+func getJwsAlg(signature string) (jwa.SignatureAlgorithm, error) {
+    signatureArr := strings.SplitN(signature, ".", 2)
+
+    headerStr, err := base64.StdEncoding.DecodeString(signatureArr[0])
+    if err != nil {
+        return jwa.HS256, err
+    }
+
+    type Header struct {
+        Alg     string     `json:"alg"`
+        Typ     string     `json:"typ"`
+    }
+
+    var header Header
+    err = json.Unmarshal([]byte(headerStr), &header)
+    if err != nil {
+        return jwa.HS256, err
+    }
+
+    var alg jwa.SignatureAlgorithm
+    switch algStr := strings.ToUpper(header.Alg); algStr {
+    case "HS256":
+            alg = jwa.HS256
+    case "HS384":
+            alg = jwa.HS384
+    case "HS512":
+            alg = jwa.HS512
+    case "RS256":
+            alg = jwa.RS256
+    case "RS384":
+            alg = jwa.RS384
+    case "RS512":
+            alg = jwa.RS512
+    case "PS256":
+            alg = jwa.PS256
+    case "PS384":
+            alg = jwa.PS384
+    default:
+            alg = jwa.HS512
+    }
+    return alg, nil
 }
 
 func parseAuthHeader(header string) (string, error) {
