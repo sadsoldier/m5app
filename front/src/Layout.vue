1234cd0e07fd738f6ad600b75d446bd8501fc752
Oleg Borodin <onborodin@gmail.com>
2020-05-29 08:52:55 +0200

    add Layout Login; minimal login logic with token

---
+++ front/src/Layout.vue
@@ -0,0 +1,60 @@
+
+<template>
+    <div id="layout">
+
+        <nav class="navbar navbar-expand-sm sticky-top navbar-dark bg-dark">
+            <button class="btn m-0 p-0">
+                <div class="mr-3">
+                    <i class="fa fa-bars fa-lg"></i>
+                </div>
+            </button>
+
+            <div class="navbar-brand">
+                <i class="fab fa-old-republic fa-lg"></i> V3
+            </div>
+
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link">{{ $t('stock') }}</router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/counter" class="nav-link">Counter</router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/about" class="nav-link">About</router-link>
+                </li>
+            </ul>
+
+            <ul class="nav justify-content-end ml-auto mr-3">
+                <li class="nav-item">
+                    <div v-on:click="logout"><i class="fas fa-sign-out-alt fa-lg"></i></div>
+                </li>
+            </ul>
+
+        </nav>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col col-10 col-sm-10 col-md-9 col-lg-8 mt-4 mx-auto">
+
+                    <slot/>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        logout() {
+            this.$store.dispatch('logout')
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
---
+++ front/src/Login.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-sm sticky-top navbar-dark bg-dark mb-4">
+            <span class="navbar-text ml-3">
+                <i class="fab fa-old-republic fa-lg"></i>
+                M5 Login
+            </span>
+
+            <ul class="nav justify-content-end ml-auto">
+            </ul>
+        </nav>
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-8 col-sm-6 col-md-4 border p-4 mt-sm-5 ml-3 mr-3">
+
+                    <form accept-charset="UTF-8" v-on:submit.prevent="login">
+
+                        <div class="form-group">
+                            <label for="username">Login name:</label>
+                            <input id="username" class="form-control" type="text" v-model="username"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input id="password" class="form-control" type="password" v-model="password"/>
+                        </div>
+
+                        <div class="text-center">
+                            <button class="btn btn-primary btn-sm" type="submit">Submit</button>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col mt-5">
+                    <hr class="justify-content-sm-center" />
+                    <div class="text-center">
+                        <small>made by <a href="http://wiki.unix7.org">oleg borodin</a></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            username: "",
+            password: ""
+        };
+    },
+    methods: {
+        login() {
+            axios
+                .post('/login', {
+                    username: this.username,
+                    password: this.password
+                })
+                .then(response => {
+                    if (response.data.error == false) {
+                        this.$store.dispatch('login')
+                        localStorage.setItem('token', response.data.result.token)
+                        this.$router.push('/')
+                    }
+                })
+                .catch(err => {})
+        }
+    }
+}
+</script>
